450c024819f5c4a123ab2486ea1ab2da2e2e7880
Ibtikar100 <android@ibtikartechs.com>
2018-09-17 19:02:14 +0200

    new

--- app/src/main/java/com/ibtikar/app/egymart/ui/activities/main/MainActivity.java
+++ app/src/main/java/com/ibtikar/app/egymart/ui/activities/main/MainActivity.java
@@ -1,21 +1,101 @@
 package com.ibtikar.app.egymart.ui.activities.main;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ibtikar.app.egymart.R;
+import com.ibtikar.app.egymart.ui.fragments.CategoriesFragment;
 import com.ibtikar.app.egymart.ui.fragments.MenueDialogFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_toolbar)
+    Toolbar toolbar;
+
+    TextView textCartItemCount;
+
+
+
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        MenueDialogFragment searchDialogFragment = new MenueDialogFragment();
-        searchDialogFragment.show(fm, "menu_dialog");
+
+
+        mHandler = new Handler(Looper.getMainLooper());
+        ButterKnife.bind(this);
+        setupActionBar();
+        getSupportFragmentManager().beginTransaction().add(R.id.main_fragment_container, new CategoriesFragment(), "").commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        final MenuItem cartItem = menu.findItem(R.id.action_cart);
+        View actionView = MenuItemCompat.getActionView(cartItem);
+        textCartItemCount = actionView.findViewById(R.id.cart_badge);
+        setupBadge();
+        actionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(cartItem);
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_cart :
+               /* FragmentManager fm = getSupportFragmentManager();
+                SearchDialogFragment searchDialogFragment = new SearchDialogFragment();
+                searchDialogFragment.show(fm, "alert"); */
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupBadge() {
+    }
+
+
+    public void setupActionBar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.icon_left);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((getSupportFragmentManager().findFragmentByTag("menu_dialog"))== null ) {
+                    FragmentManager fm = getSupportFragmentManager();
+                    MenueDialogFragment menuDialogFragment = new MenueDialogFragment();
+                    menuDialogFragment.show(fm, "menu_dialog");
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/ibtikar/app/egymart/ui/fragments/CategoriesFragment.java
+++ app/src/main/java/com/ibtikar/app/egymart/ui/fragments/CategoriesFragment.java
@@ -2,12 +2,18 @@ package com.ibtikar.app.egymart.ui.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ibtikar.app.egymart.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +25,8 @@ public class CategoriesFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.emp_lout)
+    LinearLayout loutClick;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,7 +68,19 @@ public class CategoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false);
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+        ButterKnife.bind(this,view);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loutClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.main_fragment_container, new ProductsFragment(), "").addToBackStack("").commit();
+            }
+        });
+    }
 }
